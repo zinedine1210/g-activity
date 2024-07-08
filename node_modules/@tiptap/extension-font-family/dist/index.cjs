915b2ac7b1f585987bbf2f9a8433124eb6699c63
zinedine1210 <ziddanfhdlvy12@gmail.com>
2024-07-08 21:15:45 +0700

    Coba rebase main

---
+++ node_modules/@tiptap/extension-font-family/dist/index.cjs
@@ -0,0 +1,55 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+require('@tiptap/extension-text-style');
+var core = require('@tiptap/core');
+
+const FontFamily = core.Extension.create({
+    name: 'fontFamily',
+    addOptions() {
+        return {
+            types: ['textStyle'],
+        };
+    },
+    addGlobalAttributes() {
+        return [
+            {
+                types: this.options.types,
+                attributes: {
+                    fontFamily: {
+                        default: null,
+                        parseHTML: element => { var _a; return (_a = element.style.fontFamily) === null || _a === void 0 ? void 0 : _a.replace(/['"]+/g, ''); },
+                        renderHTML: attributes => {
+                            if (!attributes.fontFamily) {
+                                return {};
+                            }
+                            return {
+                                style: `font-family: ${attributes.fontFamily}`,
+                            };
+                        },
+                    },
+                },
+            },
+        ];
+    },
+    addCommands() {
+        return {
+            setFontFamily: fontFamily => ({ chain }) => {
+                return chain()
+                    .setMark('textStyle', { fontFamily })
+                    .run();
+            },
+            unsetFontFamily: () => ({ chain }) => {
+                return chain()
+                    .setMark('textStyle', { fontFamily: null })
+                    .removeEmptyTextStyle()
+                    .run();
+            },
+        };
+    },
+});
+
+exports.FontFamily = FontFamily;
+exports["default"] = FontFamily;
+//# sourceMappingURL=index.cjs.map
