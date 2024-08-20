10a6ae54f613d8a8af0d3b5cb8d4d18abda6f73a
charlygnusa <charly.gnusa@gmail.com>
2024-08-20 21:40:06 +0700

    final reply & forward 2

--- components/Mail/Mailbox/DetailMail.jsx
+++ components/Mail/Mailbox/DetailMail.jsx
@@ -289,9 +289,8 @@ const EmailDetail = ({ account }) => {
         {
           detailMailData.Attachments && detailMailData.Attachments.length > 0 && <div className="border-t-2 flex space-x-4 py-4"><AttachmentList attachments={detailMailData.Attachments} downloadFiles={onDownloadsFile} /></div>
         }
-
         {
-          filterUID == "Drafts" || filterUID == "Sent" && <div className="mt-8 flex items-center space-x-4 text-sm">
+          filterUID == "Inbox" || filterUID == "Sent" ? <div className="mt-8 flex items-center space-x-4 text-sm">
             <button className="w-32 flex items-center justify-center space-x-2 py-1.5 text-gray-600 border border-gray-400 rounded-lg hover:bg-gray-200" onClick={handleReplyClick}>
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 017 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd"></path>
@@ -304,7 +303,7 @@ const EmailDetail = ({ account }) => {
               </svg>
               <span>Forward</span>
             </button>
-          </div>
+          </div>: null
         }
 
       </div>
